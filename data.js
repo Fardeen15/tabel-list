fe3c6deb63dcbe1ab855fd7629f8204eff2d98ea
fardeen15 <fardeenmemon17@gmail.com>
2019-04-30 05:25:11 -0700

    todo list completed

--- data.js
+++ data.js
@@ -1,48 +1,3 @@
-// var rIndex;
-// var table = document.getElementById("table");
-
-// function addHtmlTableRow() {
-//     var newRow = table.insertRow(table.length);
-//     var cell1 = newRow.insertCell(0);
-//     var cell2 = newRow.insertCell(1);
-//     var cell3 = newRow.insertCell(2);
-//     var fname = document.getElementById("fname").value;
-//     var lname = document.getElementById("lname").value;
-//     var age = document.getElementById("age").value;
-//      cell1.innerHTML = fname;
-//      cell2.innerHTML = lname;
-//      cell3.innerHTML = age;
-//     selectedRowToInput();
-// }
-
-// function selectedRowToInput() {
-//     for (var i = 0; i < table.rows.length; i++) {
-//         table.rows[i].onclick = function () {
-//             rIndex = this.rowIndex;
-//             document.getElementById("fname").value = this.cells[0].innerHTML;
-//             document.getElementById("lname").value = this.cells[1].innerHTML;
-//             document.getElementById("age").value = this.cells[2].innerHTML;
-//         }
-//     }
-// }
-// selectedRowToInput();
-
-// function editHtmlTbleSelectedRow() {
-//     var fname = document.getElementById("fname").value;
-//     var lname = document.getElementById("lname").value;
-//     var age = document.getElementById("age").value;
-//     table.rows[rIndex].cells[0].innerHTML = fname;
-//     table.rows[rIndex].cells[1].innerHTML = lname;
-//     table.rows[rIndex].cells[2].innerHTML = age;
-// }
-
-// function removeSelectedRow() {
-//     table.deleteRow(rIndex);
-//     document.getElementById("fname").value = "";
-//     document.getElementById("lname").value = "";
-//     document.getElementById("age").value = "";
-
-// }
 var nameArr = []
 var secondname = []
 var Age = []
@@ -64,115 +19,77 @@ function addHtmlTableRow() {
 
 function addData() {
     table.innerHTML = `
-    <tr >
+    <tr>
+    <th>s.no</th>
     <th>First Name</th>
     <th>Last Name</th>
     <th>Age</th>
     </tr>
     `
-    for(var i = 0; i < nameArr.length; i++){
+    for (var i = 0; i < nameArr.length; i++) {
         table.innerHTML += `
         <tr >
+        <td>${i + 1}</td>
         <td>${nameArr[i]}</td>
         <td>${secondname[i]}</td>
         <td>${Age[i]}</td>
-        <td><button onclick="removeSelectedRow(${i})">Remove</button></td>
-        <td><button onclick="editHtmlTbleSelectedRow(${i})">Edit</button></td>
+        <td><button onclick="removeSelectedRow(${i})" class="btn btn-outline-secondary">Remove</button></td>
+        <td><button onclick="editHtmlTbleSelectedRow(${i})" class="btn btn-outline-secondary">Edit</button></td>
         </tr>`
     }
+
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("age").value = "";
 }
 
 
-function removeSelectedRow(a){
+function removeSelectedRow(a) {
     console.log(a)
-nameArr.splice(a,1);
-addData()
+    console.log(nameArr)
+
+    nameArr.splice(a, 1);
+    secondname.splice(a, 1);
+    Age.splice(a, 1);
+    table.innerHTML = ""
+
+    addData()
 }
 var index;
-function editHtmlTbleSelectedRow(b){
+function editHtmlTbleSelectedRow(b) {
     document.getElementById("fname").value = nameArr[b];
     document.getElementById("lname").value = secondname[b]
     document.getElementById("age").value = Age[b]
     index = b
-    
+    document.getElementById("updateBtn").style.display = "inline";
+    document.getElementById("addBtn").style.display = "none";
     // console.log(b)
     // console.log(nameArr[b],secondname[b],Age[b])
     // var firstname = document.getElementById("fname"[b]).value
 }
 
 
-function updateRow(c) {
-    table.innerHTML += `
-    <tr >
-    <td>${nameArr[index]}</td>
-    <td>${secondname[index]}</td>
-    <td>${Age[index]}</td>
-    <td><button onclick="removeSelectedRow(${index})">Remove</button></td>
-    <td><button onclick="editHtmlTbleSelectedRow(${index})">Edit</button></td>
-    </tr>`
+function updateRow() {
+    index;
+    console.log(index)
+    var name = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var age = document.getElementById("age").value;
 
-// editHtmlTbleSelectedRow(c).innerHTML = ""
-console.log(index)
+
+    document.getElementById("updateBtn").style.display = "none";
+    document.getElementById("addBtn").style.display = "inline";
+
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("age").value = "";
+
+    nameArr.splice(index, 1, name)
+    secondname.splice(index, 1, lname)
+    Age.splice(index, 1, age)
+    addData()
 }
 
 
 
 
-
-// editHtmlTbleSelectedRow()
-
-// var students = [
-//     {
-//         name: "fardeen",
-//         class: 10,
-//         age: 15,
-//     },
-//     {
-//         name: "dilawar",
-//         class: 10,
-//         age: 16,
-//     },
-//     {
-//         name: "ghori",
-//         class: 10,
-//         age: 14,
-//     },
-// ]
-// function editRow() {
-//    document.write(`
-//         <form>
-//      Name : <input type="text" ${id = "name"} ><br><br>
-//      Class : <input type="text" ${id ="class"}><br><br>
-//      Age : <input type="text" ${id = "age"}><br><br>
-//      <input type="button" value="update" title="update" onclick="update()">
-//     </form>
-//     `)
-// }
-
-// document.write(`
-// <table>
-//     <th>name</th>
-//     <th>class</th>
-//     <th>age</th>
-//     <tr>
-//         <td>${students[0].name}</td>
-//         <td>${students[0].class}</td>
-//         <td>${students[0].age}</td>
-//         <td><input type="button" value="edit" title="edit" onclick="editRow()"></td>
-//         <td><input type="button" value="delete" title="delete" onclick=""></td></tr>
-//     <tr>
-//         <td>${students[1].name}</td>
-//         <td>${students[1].class}</td>
-//         <td>${students[1].age}</td>
-//         <td><input type="button" value="edit" title="edit" onclick="editRow()"></td>
-//         <td><input type="button" value="delete" title="delete" onclick=""></td>
-//     </tr>
-//     <tr>
-//         <td>${students[2].name}</td>
-//         <td>${students[2].class}</td>
-//         <td>${students[2].age}</td>
-//         <td><input type="button" value="edit" title="edit" onclick="editRow()"></td>
-//         <td><input type="button" value="delete" title="delete" onclick=""></td>
-//     </tr>
-// </table>
-// `)
